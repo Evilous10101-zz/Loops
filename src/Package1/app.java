bb21b68c2991ba8e1b20da31cd3baba22265cbf7
Michael Lannon <sMichaelLannon@yccc.edu>
2018-09-19 19:26:51 -0400

    first

---
+++ src/Package1/app.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class app {
+public static void main(String[] args) {
+	loop1 l = new loop1();
+	
+
+	l.createMask();
+}
+}
--- src/Package1/loop1.java
+++ src/Package1/loop1.java
@@ -1,5 +1,21 @@
 package Package1;
 
 public class loop1 {
+public void forLoopSample2() {
+	String word = "cat";
+	for(int i = 0; i < word.length(); i++)
+	{
+		System.out.println(i);
+		System.out.println(word.charAt(i));
+	}
+}
+
+public void createMask()
+{
+	String word = "Horse";
+	for(int i = 0; i < word.length(); i++)
+		System.out.print("*");
+}
+
 
 }
